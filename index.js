9095dac6d80796455d2ac34ba3da827c9129b725
Мария <Lisa@MacBook-Pro-Maria.local>
2021-03-11 18:35:43 +0300

    Функции числовых массивов. Часть 1.

---
+++ index.js
@@ -0,0 +1,5 @@
+const NumberArray = require("./lib/NumberArray")
+const arr =[5,3,2,1,11,77,600,543,221,56];
+console.log(NumberArray.min(arr)) //1
+console.log(NumberArray.max(arr)) //600
+console.log(NumberArray.avg(arr)) // 151.9
