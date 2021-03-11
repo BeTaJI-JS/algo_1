670ae5f1b844bb6346471e9e48de5a2d71a74d38
Мария <Lisa@MacBook-Pro-Maria.local>
2021-03-11 19:21:17 +0300

    Функции числовых массивов. Часть 2.( доделать)

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const arr =[5,3,2,1,11,77,600,543,221,56];
 console.log(NumberArray.min(arr)) //1
 console.log(NumberArray.max(arr)) //600
 console.log(NumberArray.avg(arr)) // 151.9
+
+console.log(NumberArray.filter(arr,"even")) // [ 2, 600, 56 ]
+console.log(NumberArray.filter(arr,"odd"))
+console.log(NumberArray.filter(arr,"div",3)) // [ 3, 600, 543 ]
