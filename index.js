268eb564b00687c7f523ab002593122110859b70
Мария <Lisa@MacBook-Pro-Maria.local>
2021-03-15 12:37:42 +0300

    class numberArray

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-// const NumberArray = require("./lib/NumberArray")
-// const arr =[5,3,2,1,11,77,600,543,221,56];
-// console.log(NumberArray.min(arr)) //1
-// console.log(NumberArray.max(arr)) //600
-// console.log(NumberArray.avg(arr)) // 151.9
+const NumberArray = require("./lib/NumberArray")
 
-// console.log(NumberArray.filter(arr,"even")) // [ 2, 600, 56 ]
-// console.log(NumberArray.filter(arr,"odd"))
-// console.log(NumberArray.filter(arr,"div",3)) // [ 3, 600, 543 ]
-// console.log(NumberArray.count(arr,3)) //1
-// console.log(NumberArray.getDictionary(arr))
-
-
-
-
-//ДЗ 1 повторение
-// const NumberArray_HomeWork = require("./lib/NumberArray_HomeWork")
-// const arr =[5,3,2,1,11,77,600,11,600,5,5,12,43,24,14,1,543,221,56];
-// console.log(NumberArray_HomeWork.min(arr))
-// console.log(NumberArray_HomeWork.max(arr))
-// console.log(NumberArray_HomeWork.avg(arr))
-// console.log(NumberArray_HomeWork.filter(arr,"div",5))
-// console.log(NumberArray_HomeWork.count(arr,5))
-// console.log(NumberArray_HomeWork.getDictionary(arr))
-
-//ДЗ 2 повторение
-
-const NumberArray_HomeWork = require("./lib/NumberArray_HomeWork_part2")
 const arr =[5,3,2,1,11,77,600,11,600,5,5,12,43,24,14,1,543,221,56];
-console.log(NumberArray_HomeWork.min(arr))
- console.log(NumberArray_HomeWork.max(arr))
- console.log(NumberArray_HomeWork.avg(arr))
- console.log(NumberArray_HomeWork.filter(arr,"div",5))
- console.log(NumberArray_HomeWork.count(arr,5))
- console.log(NumberArray_HomeWork.getDictionary(arr))
+const numberArray = new NumberArray(arr)
+console.log(numberArray.min())
+console.log(numberArray.max())
+console.log(numberArray.avg())
+console.log(numberArray.filter("div",5))
+console.log(numberArray.count(5))
+console.log(numberArray.getDictionary())
